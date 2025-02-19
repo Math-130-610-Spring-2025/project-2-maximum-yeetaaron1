d02615e6ba43143c293be26f49b6bdd11ad210ad
Aaron <cancholaron@gmail.com>
2025-02-18 23:28:11 -0800

    Maximum.Java

--- src/Maximum.java
+++ src/Maximum.java
@@ -12,18 +12,33 @@ public class Maximum {
         // Declare variables
         Scanner scan = new Scanner(System.in);
         int numA, numB, numC, max;
+
+        // Print the purpose of the program
         System.out.println("This program finds the maximum of three numbers.");
+
         // Read the first number
         System.out.print("Enter the 1st integer: ");
         numA = scan.nextInt();
+
         // Read the second number
         System.out.print("Enter the 2nd integer: ");
         numB = scan.nextInt();
+
         // Read the third number
         System.out.print("Enter the 3rd integer: ");
         numC = scan.nextInt();
+
         // Find the maximum of the three numbers
-        max = Math.max(numA, Math.max(numB, numC));
-        System.out.println("The maximum is " + max);
+        if ( numA > numB && numA > numC) {
+            System.out.println("The maximum is: " + numA);
+        } else if(numB > numA && numB > numC) {
+            System.out.println("The maximum is: " + numB);
+        } else if (numC > numA && numC > numB) {
+            System.out.println("The maximum is: " + numC);
+        } else {
+            System.out.println("There is no maximum.");
+        }
+
+        scan.nextInt();
     }
 }
